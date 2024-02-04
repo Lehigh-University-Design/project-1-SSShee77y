96487f50e54c1e3b463e4d1532a6ecb6a4d98666
Leo Chen <46356943+SSShee77y@users.noreply.github.com>
2024-02-04 18:50:45 -0500

    Made website mobile friendly

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY;
+    var vwToPx = window.innerWidth / 100;
 
     /** Title slide scroll features **/
+    var titleBlock = document.querySelector('.title-block');
     var title = document.getElementById('title');
     var subtitle = document.getElementById('subtitle');
 
     scrollPercentage = Math.min(1.0, scrollPosition / 800);
-    title.style.fontSize = 60 - 45 * scrollPercentage + 'px';
+    fontSizeChange = Math.min(60-45*scrollPercentage, (6-4.5*scrollPercentage)*vwToPx);
+    title.style.fontSize = Math.max(fontSizeChange, 20) + 'px';
     title.style.marginTop = 30 - 30 * scrollPercentage + 'vh';
     subtitle.style.opacity = 1.5 - scrollPercentage*3;
+
+    // Get the element with the class .title-block
+    if (scrollPosition >= titleBlock.clientHeight - title.clientHeight) {
+        title.style.borderBottom = '3px solid var(--color-theme-accent)';
+    } else {
+        title.style.borderBottom = 'None';
+    }
 
 });
